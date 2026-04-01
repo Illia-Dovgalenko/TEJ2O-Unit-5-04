d53794f0de7a008e2d7741e0c8073f6d78f6ab3d
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-04-01 12:56:18 -0400

    light green, yellow and red RGB

--- main.ts
+++ main.ts
@@ -5,8 +5,10 @@
  * This program light green, yellow and red RGB
 */
 
+
 // variables
 let neopixelStrip: neopixel.Strip = null
+
 
 // cleanup
 basic.clearScreen()
@@ -14,23 +16,22 @@ neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
-// show colors
-input.onButtonPressed(Button.A, function () {
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.show()
-    basic.pause(1000)
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
-    neopixelStrip.show()
-    basic.pause(1000)
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-    neopixelStrip.show()
-    basic.pause(1000)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-    basic.showIcon(IconNames.Happy)
-})
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+basic.pause(2000)
+neopixelStrip.show()
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+basic.pause(2000)
+neopixelStrip.show()
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+basic.pause(2000)
+neopixelStrip.show()
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+basic.pause(2000)
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
